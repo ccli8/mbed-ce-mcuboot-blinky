c67cf39766620d6bbf7fb1f67ad761efef486a45
Chun-Chieh Li <ccli8@nuvoton.com>
2024-09-19 10:31:41 +0800

    Migrate to Mbed CE
    1.  Common change for migration to Mbed CE from Mbed CLI 1/2
        (1) Remove *.lib originally for library import. Replace with git submodule.
        (2) Rename mbed_app.json/mbed_lib.json to mbed_app.json5/mbed_lib.json5
        (3) Add/update CMakeLists.txt
    2.  Create custom_targets directory to place custom target assets, including
        (1) custom_targets.json5
        (2) CMakeLists.txt
        (3) Upload method
    3.  Update mcuboot to v2.1.0 to get mbed cmake build support
    4.  Add target_mbed_app_start/target_mbed_app_size configs to replace unsupported
        target.mbed_app_start/target.mbed_app_size for cmake build. They expand to
        MBED_APP_START/MBED_APP_SIZE for reference by source code and linker script.
    5.  Replace sleep() with mbed_sleep()
    6.  Enable GDB debug changing GDB "load" command to "load <app>_merged.hex".
    7.  Support VS Code development

--- main.cpp
+++ main.cpp
@@ -8,72 +8,13 @@
 
 #include "bootutil/bootutil.h"
 #include "bootutil/image.h"
+#include "flash_map_backend/secondary_bd.h"
 #include "FlashIAP/FlashIAPBlockDevice.h"
 #include "blockdevice/SlicingBlockDevice.h"
 #include "drivers/InterruptIn.h"
 
-#if COMPONENT_SPIF
-#include "SPIFBlockDevice.h"
-#endif
-
-#if COMPONENT_NUSD
-#include "NuSDFlashSimBlockDevice.h"
-#endif
-
 #define TRACE_GROUP "main"
 #include "mbed-trace/mbed_trace.h"
-
-mbed::BlockDevice* get_secondary_bd(void) {
-#if TARGET_NUVOTON
-#   if TARGET_NUMAKER_IOT_M467_FLASHIAP || \
-       TARGET_NUMAKER_IOT_M467_FLASHIAP_TEST
-    static FlashIAPBlockDevice fbd(MCUBOOT_PRIMARY_SLOT_START_ADDR + MCUBOOT_SLOT_SIZE, MCUBOOT_SLOT_SIZE);
-    return &fbd;
-#   elif TARGET_NUMAKER_IOT_M467_FLASHIAP_DUALBANK || \
-       TARGET_NUMAKER_IOT_M467_FLASHIAP_DUALBANK_TEST
-    static FlashIAPBlockDevice fbd(0x80000, MCUBOOT_SLOT_SIZE);
-    return &fbd;
-#   elif TARGET_NUMAKER_IOT_M467_SPIF || \
-         TARGET_NUMAKER_IOT_M467_SPIF_TEST || \
-         TARGET_NUMAKER_IOT_M467_SPIF_BOOT80K || \
-         TARGET_NUMAKER_IOT_M467_SPIF_BOOT80K_TEST
-    /* Whether or not QE bit is set, explicitly disable WP/HOLD functions for safe. */
-    static mbed::DigitalOut onboard_spi_wp(PI_13, 1);
-    static mbed::DigitalOut onboard_spi_hold(PI_12, 1);
-    static SPIFBlockDevice spif_bd(MBED_CONF_SPIF_DRIVER_SPI_MOSI,
-                                   MBED_CONF_SPIF_DRIVER_SPI_MISO,
-                                   MBED_CONF_SPIF_DRIVER_SPI_CLK,
-                                   MBED_CONF_SPIF_DRIVER_SPI_CS);
-    static mbed::SlicingBlockDevice sliced_bd(&spif_bd, 0x0, MCUBOOT_SLOT_SIZE);
-    return &sliced_bd;
-#   elif TARGET_NUMAKER_IOT_M487_SPIF || \
-         TARGET_NUMAKER_IOT_M487_SPIF_TEST
-    /* Whether or not QE bit is set, explicitly disable WP/HOLD functions for safe. */
-    static mbed::DigitalOut onboard_spi_wp(PC_5, 1);
-    static mbed::DigitalOut onboard_spi_hold(PC_4, 1);
-    static SPIFBlockDevice spif_bd(MBED_CONF_SPIF_DRIVER_SPI_MOSI,
-                                   MBED_CONF_SPIF_DRIVER_SPI_MISO,
-                                   MBED_CONF_SPIF_DRIVER_SPI_CLK,
-                                   MBED_CONF_SPIF_DRIVER_SPI_CS);
-    static mbed::SlicingBlockDevice sliced_bd(&spif_bd, 0x0, MCUBOOT_SLOT_SIZE);
-    return &sliced_bd;
-#   elif TARGET_NUMAKER_IOT_M467_NUSD || \
-         TARGET_NUMAKER_IOT_M467_NUSD_TEST || \
-         TARGET_NUMAKER_IOT_M487_NUSD || \
-         TARGET_NUMAKER_IOT_M487_NUSD_TEST
-    /* For NUSD, use the flash-simulate variant to fit MCUboot flash map backend. */
-    static NuSDFlashSimBlockDevice nusd_flashsim;
-    static mbed::SlicingBlockDevice sliced_bd(&nusd_flashsim, 0x0, MCUBOOT_SLOT_SIZE);
-    return &sliced_bd;
-#   else
-#   error("Target not support: Block device for secondary slot")
-#   endif
-#else
-    mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
-    static mbed::SlicingBlockDevice sliced_bd(default_bd, 0x0, MCUBOOT_SLOT_SIZE);
-    return &sliced_bd;
-#endif
-}
 
 /* For Nuvoton targets, wake up from sleep through GPIO interrupt
  *
@@ -143,7 +84,7 @@ int main()
 #else
     while (!btn) {
 #endif
-        sleep();
+        mbed_sleep();
     }
 
     BlockDevice *secondary_bd = get_secondary_bd();
@@ -169,7 +110,7 @@ int main()
 #else
     while (!btn) {
 #endif
-        sleep();
+        mbed_sleep();
     }
 
 #if HAS_OTA_DL_SIM_SLOT
@@ -235,7 +176,7 @@ int main()
 #else
     while (!btn) {
 #endif
-        sleep();
+        mbed_sleep();
     }
 
     ret = boot_set_pending(false);
